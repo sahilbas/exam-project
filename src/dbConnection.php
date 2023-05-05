d1baba3de614342fa54f103123ab250191507381
Sahil Rai <sahil@example.com>
2023-05-05 17:39:27 +0530

    first commit

---
+++ src/dbConnection.php
@@ -0,0 +1,5 @@
+<?php
+//all the variables defined here are accessible in all the files that include this one
+$con= new mysqli('10.98.216.54','root','rootpassword','sqldb')or die("Could not connect to mysql".mysqli_error($con));
+
+?>
